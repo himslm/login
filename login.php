fbab82e9504003687aac4409f3c73fd99a2c9a76
Matt H. <himslm@gmail.com>
2015-06-10 10:42:04 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,2 +1,12 @@
-
+<!DOCTYPE html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <form method="post" action="processes.php">
+    <input type="text" name="user" />
+    <input type="password" name="pass" />
+    <input type="submit" name="login" value="Log In" />
+  </form>
+</body>
 
