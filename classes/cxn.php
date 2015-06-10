e2fb6d5e5a42dd6a74bce1ad8db93a51e087c21a
Matt H. <himslm@gmail.com>
2015-06-10 10:47:00 -0500

    Create cxn.php

---
+++ classes/cxn.php
@@ -0,0 +1,15 @@
+<?php
+class cxn{
+  protected $cxn;
+  private $host = '';
+  private $user = '';
+  private $pass = '';
+  private $db = '';
+  
+  function __construct(){
+    $this->cxn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+  }
+  function escape($value){
+    return $this->cxn->escape_string($value);
+  }
+}
